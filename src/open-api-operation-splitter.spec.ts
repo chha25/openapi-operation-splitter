b969b6311c6f300655c4594164dba4216cfaa301
Christoph Hammerbacher <christoph.hammerbacher@hotmail.de>
2021-11-13 23:12:05 +0100

    Add getPathItemsByOperation

--- src/open-api-operation-splitter.spec.ts
+++ src/open-api-operation-splitter.spec.ts
@@ -1,4 +1,6 @@
 import { OpenApiOperationSplitter } from './open-api-operation-splitter';
+import { OpenAPI } from 'openapi-types';
+import exp from 'constants';
 
 describe('OpenApiOperationSplitter', () => {
     it('should be parsable', async () => {
@@ -12,12 +14,40 @@ describe('OpenApiOperationSplitter', () => {
     it('should be throw exception if file do not exist', async () => {
         const openApiOperationSplitter: OpenApiOperationSplitter = new OpenApiOperationSplitter();
         await expect(openApiOperationSplitter.parse('')).rejects
-        .toThrow('Expected a file path, URL, or object. Got undefine');
+            .toThrow('Expected a file path, URL, or object. Got undefine');
     });
 
     it('should be throw excption by invalid yaml', async () => {
         const openApiOperationSplitter: OpenApiOperationSplitter = new OpenApiOperationSplitter();
         await expect(openApiOperationSplitter.parse('input/invalid_swagger.yaml')).rejects
-        .toThrow('is not a valid Swagger API definition');
+            .toThrow('is not a valid Swagger API definition');
+    });
+
+    it('shoud get operations (v3)', () => {
+        const openApiOperationSplitter: OpenApiOperationSplitter = new OpenApiOperationSplitter();
+        const api: OpenAPI.Document = {
+            "openapi": "3.0.0",
+            "info": { "title": "Sample API", "description": "Optional multiline or single-line description in [CommonMark](http://commonmark.org/help/) or HTML.", "version": "0.1.9" },
+            "servers": [{ "url": "http://api.example.com/v1", "description": "Optional server description, e.g. Main (production) server" }, { "url": "http://staging-api.example.com", "description": "Optional server description, e.g. Internal staging server for testing" }],
+            "paths": {
+                "/users": {
+                    "get": {
+                        "summary": "Returns a list of users.", "description": "Optional extended description in CommonMark or HTML.", "responses": {
+                            "200": {
+                                "description": "A JSON array of user names", "content": {
+                                    "application/json": {
+                                        "schema": { "type": "array", "items": { "type": "string" } }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        const actualOperations = openApiOperationSplitter.getPathItemsByOperation(api, "GET");
+
+        expect(actualOperations.length).toBe(1);
     });
 });
--- src/open-api-operation-splitter.ts
+++ src/open-api-operation-splitter.ts
@@ -1,10 +1,49 @@
 import SwaggerParser from '@apidevtools/swagger-parser';
-import { OpenAPI } from "openapi-types";
+import { OpenAPI, OpenAPIV2, OpenAPIV3, OpenAPIV3_1 } from 'openapi-types';
 
 export class OpenApiOperationSplitter {
-    async parse(fileName: string) {
+    async parse(fileName: string): Promise<OpenAPI.Document> {
         let api: OpenAPI.Document = await SwaggerParser.parse(fileName);
         console.log("API name: %s, Version: %s", api.info.title, api.info.version);
         return api;
+    }
+
+    getPathItemsByOperation(api: OpenAPI.Document, method: string): OpenAPI.Operation[] {
+        let operations: OpenAPI.Operation[] = [];
+        for (let pathName in api.paths) {
+            console.log(pathName);
+            const typedOperation = this.getHttpMethod(api, method);
+            if (api.paths === undefined || typedOperation === undefined) {
+                continue;
+            }
+            const pathObject = api.paths[pathName];
+            if (pathObject === undefined) {
+                continue;
+            }
+            const operationByPathObject = pathObject[typedOperation];
+            if (operationByPathObject === undefined) {
+                continue;
+            }
+            console.log(operationByPathObject);
+            operations.push(operationByPathObject)
+        }
+        return operations;
+    }
+
+    private getHttpMethod(api: OpenAPI.Document, operation: string) {
+        const apiVersion = this.getOpenApiVersion(api);
+        if (apiVersion === '3.0.0') {
+            const typedKey = operation as keyof typeof OpenAPIV2.HttpMethods;
+            return OpenAPIV2.HttpMethods[typedKey];
+        }
+    }
+
+    private getOpenApiVersion(api: OpenAPI.Document): string {
+        if ('openapi' in api) {
+            return api.openapi;
+        } else if ('swagger') {
+            return api.swagger;
+        }
+        throw new Error('Unsupported Api');
     }
 }
