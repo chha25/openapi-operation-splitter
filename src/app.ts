c633998dca476d4b43099b9d8616fdb9ea3db988
Christoph Hammerbacher <christoph.hammerbacher@hotmail.de>
2021-11-17 20:42:21 +0100

    Add app logic

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import { parse } from 'ts-command-line-args';
 import { OpenApiOperationSplitter } from './open-api-operation-splitter';
 
-try {
-    const args = parse<Arguments>({
-        inputFile: String,
-        targetFileName: String,
-        operations: { type: String, multiple: true },
-        help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide' },
-    });
-    console.log(args);
+async function main() {
+    try {
+        const args = parse<Arguments>({
+            inputFile: String,
+            targetFileName: String,
+            operations: { type: String, multiple: true },
+            help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide' },
+        });
 
-} catch (error: any) {
-    console.log(error);
+        const openApiOperationSplitter: OpenApiOperationSplitter = new OpenApiOperationSplitter();
+        const parsedApi = await openApiOperationSplitter.parse(args.inputFile);
+        const paths = openApiOperationSplitter.getPathsObjectByOperation(parsedApi, args.operations[0]);
+        parsedApi.paths = paths;
+        openApiOperationSplitter.saveApiToYaml(parsedApi, args.targetFileName);
+
+    } catch (error: any) {
+        console.log(error);
+    }
 }
 
-const openApiOperationSplitter: OpenApiOperationSplitter = new OpenApiOperationSplitter();
+main();
