8bfee542f553ed39cc16ae150ddeb15f456dabd7
Account4Office <75009171+Account4Office@users.noreply.github.com>
2022-08-19 15:01:16 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,23 @@ export async function main() {
     try {
         const args = parse<Arguments>({
             inputFile: String,
-            targetFileName: String,
+            targetFile: String,
             operations: { type: String, multiple: true },
+            ignore: {type: String, optional: true},
             help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide' },
         });
 
         const openApiOperationSplitter: OpenApiOperationSplitter = new OpenApiOperationSplitter();
         const parsedApi = await openApiOperationSplitter.parse(args.inputFile);
-        const paths = openApiOperationSplitter.getPathsObjectByOperation(parsedApi, ...args.operations);
+        const ignore = args.ignore===undefined? "" : args.ignore;
+        const paths = openApiOperationSplitter.getPathsObjectByOperation(parsedApi, ignore, ...args.operations);
+        const targetFile = args.targetFile;
         parsedApi.paths = paths;
-        await openApiOperationSplitter.saveApiToYaml(parsedApi, args.targetFileName);
+        if (targetFile.split(".")[1].toLowerCase() === "yaml") {
+            await openApiOperationSplitter.saveApiToYaml(parsedApi, targetFile);
+        } else {
+            await openApiOperationSplitter.saveApiToJson(parsedApi, targetFile);
+        }
 
     } catch (error: any) {
         console.log(error);
